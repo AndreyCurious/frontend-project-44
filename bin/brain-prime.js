7d8bf7336211d0998cb9c59d0111c6bae300b716
Andrey Didenko <ded14181998@gmail.com>
2022-11-10 19:49:20 +0500

    add game-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import gamePrime from '../games/prime-game.js';
+
+gamePrime();
--- games/prime-game.js
+++ games/prime-game.js
@@ -3,20 +3,26 @@ import { getRandomInt } from '../src/get-random.js';
 import { basisOfTheGame } from '../src/index.js';
 
 export default () => {
-  const ruleOfEven = 'Answer "yes" if the number is even, otherwise answer "no".';
-  const taskOfEven = () => {
+  const ruleOfPrime = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const taskOfPrime = () => {
     const num = getRandomInt(101);
     console.log(`${'Question: '} ${num}`);
     const random = readlineSync.question(`${'Your answer: '}`);
+    let sum = 0;
+    for (let i = 1; i <= num; i += 1) {
+      if (num % i === 0) {
+        sum += 1;
+      }
+    }
     if (
-      (num % 2 === 0 && random === 'yes')
-      || (num % 2 !== 0 && random === 'no')
+      (sum === 2 && random === 'yes')
+      || (sum !== 2 && random === 'no')
     ) {
       return 'Correct!';
     }
-    if (num % 2 === 0) {
+    if (sum === 2) {
       console.log(
-        `"${random}"${' is wrong answer ;(. Correct answer was "yes".'}`,
+        `"${random}"${" is wrong answer ;(. Correct answer was 'yes'."}`,
       );
     } else {
       console.log(
@@ -25,5 +31,5 @@ export default () => {
     }
     return 'break!';
   };
-  basisOfTheGame(ruleOfEven, taskOfEven);
+  basisOfTheGame(ruleOfPrime, taskOfPrime);
 };
